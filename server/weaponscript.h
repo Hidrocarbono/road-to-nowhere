69fb0639421812624a5f6b0e2889fc6b604e89c9
Hidrocarboneto <mateus.ananias@outlook.com>
2026-07-25 18:25:35 -0300

    fix(RTN): restore correct structs from validated source (Brother Hermes)

--- server/weaponscript.h
+++ server/weaponscript.h
@@ -1,105 +1,124 @@
 /*
- * weaponscript.h - data-driven weapon/ammo script system (Xash Weapon System)
- * Copyright (C) 2026 Brother Hermes - sistema de armas por script, por Hermes e Hidrocarboneto
- * Road to Nowhere project (Paranoia 2 based mod)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This header mirrors the script format used by Uncle Mike in Paranoia 2
- * (ammodesc.txt and weapon_*.txt) so existing mods can be imported without
- * changing their script files.
- */
+weaponscript.h - data-driven weapon/ammo script system (Xash Weapon System)
+Copyright (C) 2026 Brother Hermes - sistema de armas por script, por Hermes e Hidrocarboneto
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This header mirrors the script format used by Uncle Mike in Paranoia 2
+(ammodesc.txt and weapon_*.txt) so existing mods can be imported without
+changing their script files.
+*/
 
 #ifndef WEAPONSCRIPT_H
 #define WEAPONSCRIPT_H
 
-#include <cstdint>
+#include "const.h"
 
 #define MAX_AMMO_TYPES		64
 #define MAX_WEAPON_SPRITES	8
+#define MAX_WEAPON_NAME		64
+#define MAX_WEAPON_MODELS	64
+#define MAX_AMMO_NAME		32
+#define MAX_SOUND_PATH		64
+#define MAX_HUDSPRITE_NAME	32
+
+#define WIF_IRONSIGHT	(1<<0)
+#define WIF_AUTOAIM		(1<<1)
+#define WIF_AUTOFIRE	(1<<2)
 
 typedef struct ammoinfo_s
 {
-	char name[32];
+	char	name[MAX_AMMO_NAME];
 	int	MaxCarry;
 	int	PlayerDamage;
 	int	MonsterDamage;
 	int	Damage;
-	float Distance;
+	float	Distance;
 	int	NumShots;
-	char ShellModel[64];
-	char Missile[32];
+	char	ShellModel[MAX_WEAPON_MODELS];
+	char	Missile[MAX_AMMO_NAME];
 	int	count;
 } ammoinfo_t;
 
 typedef struct ammopickup_s
 {
-	char classname[64];
-	char type[32];
+	char	classname[MAX_WEAPON_NAME];
+	char	model[MAX_WEAPON_MODELS];
+	char	sound[MAX_SOUND_PATH];
+	char	type[MAX_AMMO_NAME];
 	int	count;
 } ammopickup_t;
 
-typedef struct
+typedef struct weaponattack_s
 {
-	float SpreadRange[2];
-	float SpreadExpand;
-	float SpreadRangeIS[2];
-	float SpreadExpandIS;
-	float PunchAngle[3];
-	float PunchAngleIS[3];
+	char	action[32];
+	float	nextattack;
+	float	PunchAngle[3];
+	float	PunchAngleIS[3];
+	float	SpreadRange[2];
+	float	SpreadExpand;
+	float	SpreadRangeIS[2];
+	float	SpreadExpandIS;
 } weaponattack_t;
 
-typedef struct
+typedef struct weaponsound_s
 {
-	char action[64];
-	float nextattack;
-	weaponattack_t at;
+	char	shootsound1[MAX_SOUND_PATH];
+	char	shootsound2[MAX_SOUND_PATH];
+	char	emptysound[MAX_SOUND_PATH];
 } weaponsound_t;
 
-typedef struct
+typedef struct weaponsprite_s
 {
-	char name[32];
-	char file[64];
+	char	name[MAX_HUDSPRITE_NAME];
+	char	file[MAX_SOUND_PATH];
 	int	x, y, width, height;
 } weaponsprite_t;
 
-typedef struct
+typedef struct weaponinfo_s
 {
-	char viewmodel[64];
-	char playermodel[64];
-	char worldmodel[64];
-	char anim_prefix[16];
+	char	viewmodel[MAX_WEAPON_MODELS];
+	char	playermodel[MAX_WEAPON_MODELS];
+	char	worldmodel[MAX_WEAPON_MODELS];
+	char	anim_prefix[32];
 	int	bucket;
 	int	bucket_position;
 	int	clip_size;
 	int	defaultammo;
-	char primary_ammo[32];
-	char secondary_ammo[32];
+	char	primary_ammo[MAX_AMMO_NAME];
+	char	secondary_ammo[MAX_AMMO_NAME];
 	int	weight;
-	float SpreadTime;
+	float	SpreadTime;
 	int	item_flags;
-	float MaxSpeed;
-	float MaxSpeedIS;
+	float	MaxSpeed;
+	float	MaxSpeedIS;
+	char	volume[16];
+	char	flash[16];
+
+	weaponattack_t	primary;
+	weaponattack_t	secondary;
+	weaponsound_t	sound;
+	weaponsprite_t	sprites[MAX_WEAPON_SPRITES];
 	int	num_sprites;
-	weaponsprite_t sprites[MAX_WEAPON_SPRITES];
-	weaponattack_t primary;
-	weaponattack_t secondary;
-	weaponsound_t sound;
 } weaponinfo_t;
 
-extern weaponinfo_t gWeaponInfo[MAX_AMMO_TYPES];
-extern ammoinfo_t gAmmoInfo[MAX_AMMO_TYPES];
-extern ammopickup_t gAmmoPickups[MAX_AMMO_TYPES];
-extern int gNumWeaponInfo;
-extern int gNumAmmoInfo;
-extern int gNumAmmoPickups;
+extern ammoinfo_t		gAmmoInfo[MAX_AMMO_TYPES];
+extern int			gNumAmmoInfo;
+extern ammopickup_t		gAmmoPickups[MAX_AMMO_TYPES];
+extern int			gNumAmmoPickups;
+extern weaponinfo_t		gWeaponInfo[MAX_AMMO_TYPES];
+extern int			gNumWeaponInfo;
+
+ammoinfo_t *WeaponScript_FindAmmo( const char *name );
+int  WeaponScript_ParseAmmoDesc( const char *filename );
+int  WeaponScript_ParseWeapon( const char *filename );
+void WeaponScript_LoadAll( void );
 
 ammoinfo_t *WeaponScript_FindAmmo( const char *name );
 weaponinfo_t *WeaponScript_FindWeapon( const char *name );
 void WeaponScript_Init( void );
-void WeaponScript_LoadAll( void );
 
 #endif // WEAPONSCRIPT_H
