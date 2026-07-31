83e80c6464db3203e8a28df3621e030ce55abc66
Hidrocarboneto <mateus.ananias@outlook.com>
2026-07-30 21:58:48 -0300

    feat(RTN/F6): item_stimulant entity header (Brother Hermes)

---
+++ server/weapons/item_stimulant.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "monsters.h"
+#include "weapons.h"
+#include "player.h"
+
+class CItemStimulant : public CBasePlayerWeapon
+{
+	DECLARE_CLASS( CItemStimulant, CBasePlayerWeapon );
+
+public:
+	CItemStimulant();
+	void Spawn() override;
+	void Precache() override;
+	int AddToPlayer(CBasePlayer *pPlayer);
+	int GetItemInfo(ItemInfo *p) const override;
+	int iItemSlot() override { return 1; }
+	int iItemPosition() override { return 6; }
+	int iMaxClip() override { return -1; }
+	int iWeight() override { return 5; }
+	int iFlags() override { return ITEM_FLAG_SELECTONEMPTY; }
+};
