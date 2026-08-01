9131c3a6ffbab581f90ca4694f847597393a2d4b
Hidrocarboneto <mateus.ananias@outlook.com>
2026-08-01 16:23:44 -0300

    feat(RTN/F8): registra NVG_Init no GameDLLInit (Brother Hermes)

--- server/game.cpp
+++ server/game.cpp
@@ -18,6 +18,7 @@
 #include "game.h"
 #include "cbase.h"
 #include "weaponscript.h"
+#include "nvg_controller.h"
 #include "client.h"
 #include "user_messages.h"
 #include "sv_materials.h"
@@ -514,6 +515,7 @@ void GameDLLInit( void )
 
 	// Brother Hermes - Road to Nowhere: register weapon script system console commands
 	WeaponScript_Init();
+	NVG_Init();
 	CVAR_REGISTER (&displaysoundlist);
 
 	CVAR_REGISTER (&teamplay);
