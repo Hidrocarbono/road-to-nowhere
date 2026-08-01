9b161b8269c2f40d2f9a438f58ad20feeb71fbd4
Hidrocarboneto <mateus.ananias@outlook.com>
2026-08-01 09:11:22 -0300

    fix(RTN/F6): iItemPosition 5 (Brother Hermes)

--- game_shared/weapons/stimulant.h
+++ game_shared/weapons/stimulant.h
@@ -13,7 +13,7 @@ public:
 	CStimulantWeaponContext(std::unique_ptr<IWeaponLayer> &&layer);
 
 	int iItemSlot() override { return 1; }
-	int iItemPosition() override { return 6; }
+	int iItemPosition() override { return 5; }
 	int iMaxClip() override { return 1; }          // 1 use per item
 	int iMaxAmmo1() override { return -1; }
 	const char *pszAmmo1() override { return NULL; }
