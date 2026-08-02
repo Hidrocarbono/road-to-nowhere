57de74fe5830da28dafbc7759844af7b19bdfc3b
Hidrocarboneto <mateus.ananias@outlook.com>
2026-08-02 11:31:30 -0300

    fix(RTN/F9): remove include circular hud.h do header (padrao health.h) (Brother Hermes)

--- client/hud_rtn_items.h
+++ client/hud_rtn_items.h
@@ -1,7 +1,8 @@
 #pragma once
-#include "hud.h"
 
 // RTN F9: HUD lateral esquerdo com doses de estimulante (V) e painkiller (H).
+// NAO incluir hud.h aqui (include circular!). Este header eh incluido PELO hud.h
+// apos a definicao de CHudBase, igual ao health.h/ammo.h. O .cpp inclui hud.h primeiro.
 // Estilo Paranoia 2: icone + contador fixos na lateral esquerda da tela.
 // Recebe gmsgRTNItems (2 shorts: doses estimulante, doses painkiller).
 class CHudRTNItems : public CHudBase
